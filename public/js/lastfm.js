cebd833b091e1d054624a0d7bad2a5335906abf9
Josh Trommel <joshtrommel@gmail.com>
2016-01-25 02:00:12 -0800

    Set up lastfm requests and added home, as well as improved styling

--- public/js/lastfm.js
+++ public/js/lastfm.js
@@ -4,3 +4,15 @@ var lastfm = new LastFmNode({
   api_key: 'e2a6b6788c47485e6428a3dc6c1bfdd3',
   secret: '3e1b84dc73b310e41dbbde2daa5eb1d4',
 });
+
+lastfm.request('artist.getInfo', {
+  artist: "Wilco",
+  handlers: {
+    success: function(data) {
+      console.log(data.artist.bio.summary);
+    },
+    error: function(err) {
+      console.log("Error: " + err);
+    }
+  }
+})
